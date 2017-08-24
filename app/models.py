d0d42b998ab026dccc43baad179a08ef79e39cf2
zhaogongzi <zhaogongzimail@163.com>
2017-08-24 19:55:13 +0800

    commit 7.0

--- app/models.py
+++ app/models.py
@@ -97,6 +97,7 @@ class Follow(db.Model):
     followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
                             primary_key=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -138,6 +139,8 @@ class User(UserMixin, db.Model):
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(
                 self.email.encode('utf-8')).hexdigest()
+
+        self.follow(self)
 
     @property
     def password(self):
@@ -236,6 +239,15 @@ class User(UserMixin, db.Model):
             except IntegrityError:
                 db.session.rollback()
 
+    #更新数据库，用户关注自己
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
+
     #关注关系的辅助方法
     def follow(self, user):
         if not self.is_following(user):
@@ -254,6 +266,12 @@ class User(UserMixin, db.Model):
     def is_followed_by(self, user):
         return self.followers.filter_by(
             follower_id=user.id).first() is not None
+
+    #联结查询
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id)\
+               .filter(Follow.follower_id == self.id)
 
     def __repr__(self):
         return '<User %r>' % self.username
